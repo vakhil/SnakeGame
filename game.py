7366d7bf17fd9061efbfd1b0d1fc280dc1348b21
Akhil Vemula <akhilvemula95@gmail.com>
2023-08-27 04:31:08 +0530

    Added snake positon code

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import sys
 pygame.init()
 
 
+snake_pos = [100, 50]
 
 # Set up the display
 screen_width = 800
@@ -30,8 +31,6 @@ grid = [[background_asset.get_rect(topleft=(x, y)) for x in range(0, screen_widt
 
 class Snake :
 
-    def __init__(self) -> None:
-         self.body_cells = []
 
     def __init__(self, snake_head_x, snake_head_y,screen) :
         self.snake_length = 5
@@ -44,6 +43,7 @@ class Snake :
 
     def draw_snake_on_screen(self):
         index = 0
+        print("OK")
         # This is the snake head. Color it with RED
         for body_part in self.body_cells:
             if index == 0 :
@@ -63,7 +63,7 @@ class Snake :
         index = 0
         head = self.body_cells[0]
         print(head.topleft[0] +10)
-        self.generate_snake_body(head.topleft[0] +10,head.topleft[1]+10)
+        self.generate_snake_body(head.topleft[0] +2,head.topleft[1])
 
 
     def get_snake_body(self):
@@ -75,6 +75,11 @@ class Snake :
 selected_cell = None
 running = True
 snake = None
+snake_speed = 10  # Adjust this value to control snake speed
+clock = pygame.time.Clock()
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -84,15 +89,19 @@ while running:
                 for cell in row:
                     if cell.collidepoint(event.pos):
                         selected_cell = cell
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                current_direction = 0
+            elif event.key == pygame.K_RIGHT:
+                current_direction = 1
+            elif event.key == pygame.K_DOWN:
+                current_direction = 2
+            elif event.key == pygame.K_LEFT:
+                current_direction = 3
 
     # Clear the screen
     screen.fill(BLACK)
 
-    #Draw the snake if a click has been made on the screen 
-    if snake :
-        print("The snake has been created")
-        # snake.update_snake_position()
-        snake.draw_snake_on_screen()
         
 
 
@@ -101,7 +110,13 @@ while running:
     for row in grid:
         for cell in row:
             pygame.draw.rect(screen, WHITE, cell)
-            
+
+    #Draw the snake if a click has been made on the screen 
+    if snake :
+        print("The snake has been created")
+        snake.update_snake_position()
+        snake.draw_snake_on_screen()
+
 
     
 
@@ -110,12 +125,12 @@ while running:
     #     pygame.draw.rect(screen, RED, selected_cell)
            
     #Draw Snake Body
-    if selected_cell:
+    if selected_cell and not snake:
         snake = Snake(selected_cell.topleft[0],selected_cell.topleft[1],screen)
-        # snake.drawOnScreen()
-        # for bodyParts in snake.get_snake_body() :
-        #     pygame.draw.rect(screen,BLACK,bodyParts)
 
+
+    # Control the game speed
+    clock.tick(snake_speed)
     # Update the display
     pygame.display.flip()
 
